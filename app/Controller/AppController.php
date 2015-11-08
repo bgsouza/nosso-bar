633d41701a7e90df2e2ebdc59538d12d33fe3d49
Felipe Pena Aguiar Marques <felipe.marques@gennialedesign.com.br>
2015-11-08 02:40:58 -0200

    atualizacao pagamentos

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,7 +33,6 @@ App::uses('Controller', 'Controller');
 class AppController extends Controller {
 
   public $components = array(
-        'DebugKit.Toolbar',
         'Flash',
         'Auth' => array(
             'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
